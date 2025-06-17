6663e3cba8e966da681a9eb60e165b6cbe401e33
Jenny Moon <jmoon981107@gmail.com>
2025-06-17 19:48:51 +0900

    명언위치수정

--- src/Components/MainPage.js
+++ src/Components/MainPage.js
@@ -5,9 +5,9 @@ const MainPage = ({user,onLogout}) => {
     return (
         <div>
         <div className="main-page">
+            <Quote/>
             <h2>{user}님 반가워요</h2>
             <button onClick={()=>{onLogout('')}}>로그아웃</button>
-            <Quote/>
         </div>
         </div>
     );
